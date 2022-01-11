8ed88a65e0fe49f9da70f56b5fd596ec19cfc331
Devilharsha <95213910+Devilharsha@users.noreply.github.com>
2022-01-11 08:42:48 +0530

    Update alive.py

--- Pikachu/modules/alive.py
+++ Pikachu/modules/alive.py
@@ -3,19 +3,19 @@ import re, os
 from Pikachu.events import register
 from Pikachu import telethn as tbot
 from Pikachu import telethn as tgbot
-PHOTO = "https://telegra.ph/file/0ac1f1b5c5b2376a14f3f.jpg"
+PHOTO = "https://telegra.ph/file/7b0e22a3629295f34988d.jpg"
 @register(pattern=("/alive"))
 async def awake(event):
   PIKACHU = event.sender.first_name
-  PIKACHU = "**♡ I,m Pikachu** \n\n"
+  PIKACHU = "**♡ I,m infernipe** \n\n"
   PIKACHU += "**♡ I'm Working Properly**\n\n"
-  PIKACHU += "**♡ pikachu : 2.2 LATEST**\n\n"
-  PIKACHU += "**♡ My Master :** [Manjeet](t.me/Hayat_Murat_30)\n\n"
+  PIKACHU += "**♡ infernipe: 2.2 LATEST**\n\n"
+  PIKACHU += "**♡ My Master :** [harsha](@harshahero)\n\n"
   PIKACHU += "**♡ Telethon Version : 1.23.0**\n\n"
   PIKACHU += "**♡ Kali Linux : yes**\n\n"
   PIKACHU += "**♡ Python : 3.9.7**\n\n"
   PIKACHU += "**♡ Database status : Functional**\n\n"
-  BUTTON = [[Button.url("𝙎𝙐𝙋𝙋𝙊𝙍𝙏", "https://t.me/PikachuX_Support"), Button.url("𝙐𝙋𝘿𝘼𝙏𝙀", "https://t.me/PikachuX_logs")]]
+  BUTTON = [[Button.url("𝙎𝙐𝙋𝙋𝙊𝙍𝙏", "https://t.me/NovusSupport"), Button.url("𝙐𝙋𝘿𝘼𝙏𝙀", "https://t.me/NovusUpdates")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=PIKACHU,  buttons=BUTTON)
 
 
